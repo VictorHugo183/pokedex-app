5b4e32a11adb341560ae457a7d028627ac9311b0
VICTOR HUGO Nascimento <victorhugovaranda@hotmail.com>
2020-06-01 23:52:07 +0100

    Updates

---
+++ precache-manifest.6afabcc6f22036bf2a5d5df54091cbfd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab85d35c54de0eade4eecdfb15239604",
+    "url": "/pokedex-app/index.html"
+  },
+  {
+    "revision": "b9b438a7ec7df5955d2e",
+    "url": "/pokedex-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "742e3d5a2c3c7bc081bb",
+    "url": "/pokedex-app/static/css/main.db2c393d.chunk.css"
+  },
+  {
+    "revision": "b9b438a7ec7df5955d2e",
+    "url": "/pokedex-app/static/js/2.54feaac0.chunk.js"
+  },
+  {
+    "revision": "f3c389cd0cd8440ad61ba2efd5bf6d71",
+    "url": "/pokedex-app/static/js/2.54feaac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742e3d5a2c3c7bc081bb",
+    "url": "/pokedex-app/static/js/main.4c2e379e.chunk.js"
+  },
+  {
+    "revision": "2b6ac07db6f787b5faa5",
+    "url": "/pokedex-app/static/js/runtime-main.f16551d8.js"
+  },
+  {
+    "revision": "201d2559eb3036bbb8457050d899bfa6",
+    "url": "/pokedex-app/static/media/question_icon.201d2559.png"
+  }
+]);
